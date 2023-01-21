689c75bbef0d1f1e4e5d3ca0b6d0ebe66e16629f
Alin <alin@gmail.com>
2023-01-22 01:50:24 +0200

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+
+import { BallTriangle } from "react-loader-spinner";
+
+
 
 function App() {
+
+
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    try {
+      //https://api.themoviedb.org/3/movie/popular?api_key=0577c614ef28d117c73e415b1d7fbb1a&language=en-US&page=1
+      const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=0577c614ef28d117c73e415b1d7fbb1a&language=en-US&page=1");
+      const data = await response.json();
+      console.log(data);
+
+    } catch (error) {
+
+    }
+  }
+
+
+  useEffect(() => {
+
+    const timeout = setTimeout(() => {
+      setLoading(true);
+      fetchMovies();
+    }, 1000)
+
+    return () => {
+      clearTimeout(timeout);
+    }
+
+    setLoading(false);
+  }, [])
+
+
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+
+      {!loading && <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#000dff"
+        ariaLabel="ball-triangle-loading"
+        wrapperClass={{}}
+        wrapperStyle=""
+        visible={true}
+      />}
+
+      {loading &&
+        <h1>Hello</h1>}
+
+
+
+
     </div>
   );
 }
